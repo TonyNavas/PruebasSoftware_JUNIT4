14b5895ea67c5616e9b9b2a1ebc6565dafc7d144
TonyNavas <tonynavas777@gmail.com>
2021-11-17 19:23:43 -0600

    Agregué la segunda prueba para VENDEDOR

--- src/test/java/org/geronimo/EmpleadoTest.java
+++ src/test/java/org/geronimo/EmpleadoTest.java
@@ -8,5 +8,6 @@ public class EmpleadoTest {
     public void PruebaEmpleado(){
         Empleado EP = new Empleado();
         Assert.assertEquals(1360.0, EP.calcularSalarioBruto("VENDEDOR", 2000,8), 0.001);
+        Assert.assertEquals(1260.0, EP.calcularSalarioBruto("VENDEDOR", 1500,3), 0.001);
     }
 }
