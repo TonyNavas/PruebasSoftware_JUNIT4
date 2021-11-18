c659d78e819a70c0c6009caf619d2f3b0c68ff20
TonyNavas <tonynavas777@gmail.com>
2021-11-17 19:22:15 -0600

    Agregue la primera prueba para VENDEDOR

--- src/test/java/org/geronimo/EmpleadoTest.java
+++ src/test/java/org/geronimo/EmpleadoTest.java
@@ -1,10 +1,12 @@
 package org.geronimo;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class EmpleadoTest {
  @Test
     public void PruebaEmpleado(){
         Empleado EP = new Empleado();
+        Assert.assertEquals(1360.0, EP.calcularSalarioBruto("VENDEDOR", 2000,8), 0.001);
     }
 }
