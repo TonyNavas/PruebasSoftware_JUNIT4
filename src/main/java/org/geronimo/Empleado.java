8c23955baa4a35fdb2233fc35831aaac2393a657
TonyNavas <tonynavas777@gmail.com>
2021-11-17 19:03:40 -0600

    Agregue el caso en que el empleado sea Encargado

--- src/main/java/org/geronimo/Empleado.java
+++ src/main/java/org/geronimo/Empleado.java
@@ -3,7 +3,7 @@ package org.geronimo;
 public class Empleado {
     float calcularSalarioBruto(String tipo_Empleado, float ventas_Mes, float Horas_extras){
 
-        if (tipo_Empleado == "vendedor"){
+        if (tipo_Empleado == "VENDEDOR"){
             float SalarioMensual = 1000;
             float SalarioTotal = 0;
             float Horas_Trabajo;
@@ -11,13 +11,34 @@ public class Empleado {
 
             if (ventas_Mes >= 1000){
                 SalarioTotal = SalarioMensual + 100;
+            }
             if (ventas_Mes >= 1500){
                 SalarioTotal = SalarioMensual + 200;
             }
             Horas_Trabajo = Horas_extras * 20;
             T_vendedor = SalarioTotal + Horas_Trabajo;
             return T_vendedor;
-            }
         }
+            else if (tipo_Empleado == ("ENCARGADO")){
+                float SalarioMensual = 1500;
+                float SalarioTotal = 0;
+                float Horas_Trabajo;
+                float T_encargado;
+
+                if (ventas_Mes < 1000){
+                    Horas_Trabajo = Horas_extras * 20;
+                    return SalarioMensual + Horas_Trabajo;
+                }
+                if (ventas_Mes >= 1000){
+                    SalarioTotal = SalarioMensual + 100;
+                }
+                if (ventas_Mes >= 1500){
+                    SalarioTotal = SalarioMensual + 200;
+                }
+                Horas_Trabajo = Horas_extras * 20;
+                T_encargado = SalarioTotal + Horas_Trabajo;
+                return T_encargado;
+            }
+            return 0;
     }
 }
